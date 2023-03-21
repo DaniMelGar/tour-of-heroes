a70f7eadcc051a49f811bcf73f259c8b1c304cfe
Daniel Melero <daniel.melero@dedalus.eu>
2023-03-21 11:00:10 +0100

    part 5: navigation of tuto completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,22 +10,9 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   //heroes: Hero[] = []
   $hero = this.heroService.getHeroes()
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    //this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
 
 }
